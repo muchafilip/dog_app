40eacf6d9ead4315bae49296d5b34e87b33ce849
Filip Mucha <40231442+muchafilip@users.noreply.github.com>
2020-04-20 17:25:33 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 
 app.config['MONGODB_SETTINGS'] = {
-    'host': 'mongodb+srv://filo123:drobcek123Drobcek@cluster0-hluxw.gcp.mongodb.net/test?retryWrites=true&w=majority'
+    'host': 'mongodb+srv://<user>:<password>@cluster0-hluxw.gcp.mongodb.net/test?retryWrites=true&w=majority'
 }
 
 initialize_db(app)
@@ -22,47 +22,3 @@ initialize_routes(api)
 # app.register_blueprint(markers)
 
 app.run()
-
-
-# from flask import Blueprint, request, Flask, Response
-# from database.db import initialize_db
-# from database.models import Marker
-
-# uri ='mongodb+srv://filo123:drobcek123Drobcek@cluster0-hluxw.gcp.mongodb.net/test?retryWrites=true&w=majority'
-# @main.route('/markers')
-# def get_events():
-#     markers = Marker.objects().to_json()
-#     return Response(markers, mimetype="application/json", status=200)
-
-# +@app.route('/markers')
-# +def get_markers():
-# +    markers = Marker.objects().to_json()
-# +    return Response(markers, mimetype="application/json", status=200)
-
-
-# @main.route('/event', methods=['POST'])
-# def add_event():
-#     event = mongo.db.event
-
-#     lat = request.json['lat']
-#     lon = request.json['lon']
-
-#     event_id = event.insert({'lat': lat, 'lon': lon})
-#     new_event = event .find_one({'_id': event_id})
-
-#     output = {'lat': new_event['lat'], 'lon': new_event['lon']}
-
-#     return jsonify({'result': output})
-
-
-# @main.route('/event/<id>', methods=['GET'])
-# def get_event():
-#     pass
-
-
-# @main.route('/event/<id>/img', methods=['GET', 'POST'])
-# def image():
-#     if 'image' in request.files:
-#         image = request.files['image']  # binary data
-#         mongo.save_file(image.filename, image)
-#         mongo.db.users.insert({'username': request.form.get()})
